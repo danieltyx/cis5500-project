4c8ee5b4c76ff435b47f5126eee3c22f7c00e09f
Yuxuan Tian <danieltian.yx@gmail.com>
2025-05-03 15:23:59 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,21 @@ import TeamsPage from './pages/TeamsPage';
 import PlayersPage from './pages/PlayersPage';
 import GamesPage from './pages/GamesPage';
 import SignupPage from './pages/SignupPage';
+import Login from './components/Login';
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
         <NavBar />
-        {/* <Switch>
+        <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/players" component={PlayersPage} />
           <Route path="/teams" component={TeamsPage} />
           <Route path="/games" component={GamesPage} />
           <Route path="/signup" component={SignupPage} />
-        </Switch> */}
+          <Route path="/login" component={Login} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
