8f7c5a148de98c4ec095f38198807d8603f16c8d
Yuxuan Tian <danieltian.yx@gmail.com>
2025-05-09 17:26:58 -0400

    Update HomePage.js
    Co-Authored-By: Nathan Zhang <23132137+nathanhzh@users.noreply.github.com>

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../style/HomePage.css";
 
-//DONE
+//DONE1
 function HomePage() {
   return (
     <div className="home-page">
