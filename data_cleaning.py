bacc322a50d8ffb189d22ab486cb99e08972f8e4
blin19 <bjlin19@wharton.upenn.edu>
2025-05-09 18:48:55 -0400

    Create data_cleaning.py

---
+++ data_cleaning.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# Cleaning game_plays_players.csv
+# Access our data and fill in na data
+df = pd.read_csv('game_plays_players.csv')
+df.drop_duplicates(inplace=True)
+df.dropna(subset=['player_id', 'game_id', 'play_id'], inplace=True)
+df.to_csv('game_plays_players.csv', index=False)
+
+# Cleaning game_plays.csv
+df = pd.read_csv('game_plays.csv')
+df.drop_duplicates(inplace=True)
+df.dropna(subset=['play_id', 'game_id'], inplace=True)
+df.to_csv('game_plays.csv', index=False)
+
+# Cleaning player_info.csv
+df = pd.read_csv('player_info.csv')
+df.drop_duplicates(inplace=True)
+df.dropna(subset=['player_id'], inplace=True)
+df.to_csv('player_info.csv', index=False)
+
+# Cleaning team_info.csv
+df = pd.read_csv('team_info.csv')
+df.drop_duplicates(inplace=True)
+df.dropna(subset=['team_id'], inplace=True)
+df.to_csv('team_info.csv', index=False)
